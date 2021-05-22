0c0c958bea7d7981a18d67e6406ff6cb0964e808
raulpy271 <56479285+raulpy271@users.noreply.github.com>
2021-05-22 12:48:59 -0300

    Create .alias.sh

---
+++ .alias.sh
@@ -0,0 +1,13 @@
+# My terminal alias
+
+alias :q=exit
+alias :q!=poweroff
+alias py=python3
+alias gitlog='git log --all --graph'
+alias prolog=swipl
+alias ls='ls --block-size=M' 
+alias pye='. bin/activate'
+alias ocaml='rlwrap ocaml'
+
+set -o vi
+
