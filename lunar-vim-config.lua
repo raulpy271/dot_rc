2b6e354011a135a9c9f88df3fd2533124c593525
Raul <56479285+raulpy271@users.noreply.github.com>
2025-01-12 14:20:35 -0300

    Create lunar-vim-config.lua

---
+++ lunar-vim-config.lua
@@ -0,0 +1,27 @@
+-- My Lunar Vim configuration
+--
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- Settings the colorscheme from hours
+if tonumber(os.date("%H")) > 18 then
+  lvim.colorscheme = "industry"
+else
+  lvim.colorscheme = "lunar"
+end
+
+-- Changing plugins config
+lvim.builtin.nvimtree.setup.view.side = "right"
+lvim.builtin.telescope.defaults.initial_mode = "normal"
+lvim.builtin.telescope.defaults.border = true
+lvim.builtin.telescope.defaults.borderchars = { "-", "|", "-", "|", "+", "+", "+", "+" }
+
+-- Setting NeoVim Mappings
+--
+-- Go back in tagstack. It comeback to when the cursor was previusly
+lvim.keys.normal_mode["gb"] = "<C-t>"
+-- CTRL-V in insert paste the clipboard
+vim.api.nvim_set_keymap("i", "<c-v>", '<ESC>"+pa', { noremap = true })
